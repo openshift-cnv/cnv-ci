d7674531ba774d35b76deca6112675d0a9bc589b
Liora Milbaum <lmilbaum@redhat.com>
2020-01-09 10:00:37 +0200

    Using variables

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Boostrapping cluster"
+HCO_REGISTRY_IMAGE='quay.io/openshift-cnv-dev/container-native-virtualization-hco-bundle-registry:v2.2.0-185'
+
+echo "Boostrapping"
 unset NAMESPACE
-curl https://raw.githubusercontent.com/kubevirt/hyperconverged-cl
-uster-operator/master/deploy/deploy_imageregistry.sh | \
-HCO_REGISTRY_IMAGE=quay.io/openshift-cnv-dev/container-native-virtualization-hco-bundle-registry:v2.2.0-185 \
+curl https://raw.githubusercontent.com/kubevirt/hyperconverged-cluster-operator/master/deploy/deploy_imageregistry.sh | \
+HCO_REGISTRY_IMAGE=$HCO_REGISTRY_IMAGE \
 HCO_VERSION=2.2.0 \
 HCO_CHANNEL=2.2 \
 TARGET_NAMESPACE=openshift-cnv \
