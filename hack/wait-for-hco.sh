4cbedc9a55c9c6f2deee7e6b2e6ca3ee9f0dc59b
Zvi Cahana <zvic@il.ibm.com>
2021-02-22 15:12:25 +0200

    Fix kubevirt-config namespace mismatch
    Signed-off-by: Zvi Cahana <zvic@il.ibm.com>

--- hack/wait-for-hco.sh
+++ hack/wait-for-hco.sh
@@ -21,7 +21,7 @@ echo "waiting for HyperConverged operator CRD to be created"
 $SCRIPT_DIR/retry.sh 30 10 "oc get crd hyperconvergeds.hco.kubevirt.io"
 
 echo "checking if HyperConverged operator CR already exists"
-if [ "$(oc get HyperConverged kubevirt-hyperconverged -n "${TARGET_NAMESPACE}" --no-headers | wc -l)" -eq 0 ]; then
+if [ "$(oc get HyperConverged kubevirt-hyperconverged -n $TARGET_NAMESPACE --no-headers | wc -l)" -eq 0 ]; then
 
 	echo "creating HyperConverged operator CR"
 	
@@ -30,18 +30,23 @@ apiVersion: hco.kubevirt.io/v1beta1
 kind: HyperConverged
 metadata:
   name: kubevirt-hyperconverged
-  namespace: "${TARGET_NAMESPACE}"
+  namespace: $TARGET_NAMESPACE
 spec:
   BareMetalPlatform: true
 EOF
 
 fi
 
-if [[ "${KUBEVIRT_RELEASE}" =~ 0.34 ]]; then
+if [[ "$KUBEVIRT_RELEASE" =~ 0.34 ]]; then
   echo "checking KubeVirt config map already exists"
-  if [ "$(oc get ConfigMap kubevirt-config -n "${TARGET_NAMESPACE}" --no-headers | wc -l)" -eq 0 ]; then
+  if [ "$(oc get ConfigMap kubevirt-config -n $TARGET_NAMESPACE --no-headers | wc -l)" -eq 0 ]; then
     echo "creating KubeVirt config map"
-    oc create -n "${TARGET_NAMESPACE}" -f "https://storage.googleapis.com/kubevirt-prow/devel/release/kubevirt/kubevirt/v0.34.2/manifests/testing/kubevirt-config.yaml"
+
+    KUBEVIRT_CONFIG_FILE="kubevirt-config.yaml"
+    curl -Lo $KUBEVIRT_CONFIG_FILE "https://storage.googleapis.com/kubevirt-prow/devel/release/kubevirt/kubevirt/$KUBEVIRT_RELEASE/manifests/testing/kubevirt-config.yaml"
+    sed -i "s/namespace: kubevirt/namespace: $TARGET_NAMESPACE/" $KUBEVIRT_CONFIG_FILE
+
+    oc create -f $KUBEVIRT_CONFIG_FILE
   fi
 fi
 
