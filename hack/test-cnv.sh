4a5a8a06c45f4dbfbc75283187e55ae76f6207b5
Zvi Cahana <zvic@il.ibm.com>
2021-02-24 11:34:28 +0200

    Bump version-mapping.json
    Signed-off-by: Zvi Cahana <zvic@il.ibm.com>

--- hack/test-cnv.sh
+++ hack/test-cnv.sh
@@ -2,6 +2,23 @@
 
 set -euxo pipefail
 
+echo "get matching kubevirt release from the build"
+VIRT_OPERATOR_IMAGE=$(oc get deployment virt-operator -n ${TARGET_NAMESPACE} -o jsonpath='{.spec.template.spec.containers[0].image}' |
+  sed 's|registry.redhat.io/container-native-virtualization/|brew.registry.redhat.io/rh-osbs/container-native-virtualization-|')
+KUBEVIRT_TAG=$(oc image info -a /tmp/authfile.new ${VIRT_OPERATOR_IMAGE} -o json | jq '.config.config.Labels["upstream-version"]')
+KUBEVIRT_RELEASE=v$(echo ${KUBEVIRT_TAG} | awk -F '-' '{print $1}' | tr -d '"')
+if [[ ${KUBEVIRT_TAG} == *"rc"* ]]; then
+  KUBEVIRT_TESTS_URL=https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_RELEASE}/tests.test
+  if ! curl --output /dev/null --silent --head --fail "${KUBEVIRT_TESTS_URL}"; then
+    # First checking if the official release exists (without "rc"). If not - use the release candidate version.
+    KUBEVIRT_RELEASE=v$(echo ${KUBEVIRT_TAG} | awk -F '-' '{print $1"-"$2}' | tr -d '"')
+  fi
+fi
+
+trap 'rm -rf /tmp/authfile*' EXIT SIGINT SIGTERM
+
+echo "Kubevirt release in use is: ${KUBEVIRT_RELEASE}"
+
 echo "downloading the test binary"
 BIN_DIR="$(pwd)/_out" && mkdir -p "${BIN_DIR}"
 export BIN_DIR
@@ -17,6 +34,40 @@ echo "waiting for testing infrastructure to be ready"
 oc wait deployment cdi-http-import-server -n "${TARGET_NAMESPACE}" --for condition=Available --timeout=10m
 oc wait pods -l "kubevirt.io=disks-images-provider" -n "${TARGET_NAMESPACE}" --for condition=Ready --timeout=10m
 
+skip_tests+=('\[QUARANTINE]')
+skip_tests+=('Slirp Networking')
+skip_tests+=('with CPU spec')
+skip_tests+=('with TX offload disabled')
+skip_tests+=('with cni flannel and ptp plugin interface')
+skip_tests+=('with ovs-cni plugin')
+skip_tests+=('SRIOV')
+skip_tests+=('with EFI')
+skip_tests+=('Operator')
+skip_tests+=('GPU')
+skip_tests+=('DataVolume Integration')
+skip_tests+=('test_id:3468')
+skip_tests+=('test_id:3466')
+skip_tests+=('test_id:1015')
+skip_tests+=('rfe_id:393')
+skip_tests+=('test_id:4659')
+
+# Skipping VM Rename tests, which are failing due to a bug in KMP.
+skip_tests+=('test_id:4646')
+skip_tests+=('test_id:4647')
+skip_tests+=('test_id:4654')
+skip_tests+=('test_id:4655')
+skip_tests+=('test_id:4656')
+skip_tests+=('test_id:4657')
+skip_tests+=('test_id:4658')
+skip_tests+=('test_id:4659')
+
+
+skip_regex=$(printf '(%s)|' "${skip_tests[@]}")
+skip_arg=$(printf -- '--ginkgo.skip=%s' "${skip_regex:0:-1}")
+
+
+mkdir -p "${ARTIFACTS_DIR}"
+
 echo "starting tests"
 ${TESTS_BINARY} \
     -cdi-namespace="$TARGET_NAMESPACE" \
@@ -27,10 +78,10 @@ ${TESTS_BINARY} \
     -ginkgo.focus='(rfe_id:1177)|(rfe_id:273)|(rfe_id:151)' \
     -ginkgo.noColor \
     -ginkgo.seed=0 \
-    -ginkgo.skip='(Slirp Networking)|(with CPU spec)|(with TX offload disabled)|(with cni flannel and ptp plugin interface)|(with ovs-cni plugin)|(test_id:1752)|(SRIOV)|(with EFI)|(Operator)|(GPU)|(DataVolume Integration)|(test_id:3468)|(test_id:3466)|(test_id:1015)|(rfe_id:393)' \
     -ginkgo.slowSpecThreshold=60 \
     -ginkgo.succinct \
     -oc-path="$(which oc)" \
     -kubectl-path="$(which oc)" \
     -utility-container-prefix=quay.io/kubevirt \
-    -test.timeout=2h
+    -test.timeout=2h \
+    "${skip_arg}"
