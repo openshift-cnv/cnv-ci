5009e66b1da8636eaa3c2c8204824ea7dde6e8ac
Simone Tiraboschi <stirabos@redhat.com>
2021-08-31 14:03:47 +0200

    [release-4.9] Optionally deploy and upgrade from production (#39)
    Enhance the deploy and upgrade scripts
    so that they can also be used
    to deploy and upgrade using only
    the production index image.
    
    This is a manual cherry-pick of #36
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- hack/disable-default-catalog-source.sh
+++ hack/disable-default-catalog-source.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 set -euxo pipefail
+# TODO: test also the opposite case before merging
+PRODUCTION_RELEASE=${PRODUCTION_RELEASE:-true}
 
-echo "disabling default catalog source"
-oc patch OperatorHub cluster --type json -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
+if [ "$PRODUCTION_RELEASE" != "true" ]; then
+  echo "disabling default catalog source"
+  oc patch OperatorHub cluster --type json -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
+fi
