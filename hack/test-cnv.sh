b83b1cb6c266e4d4c86d8c099a544aac644d1e2d
orenc1 <ocohen@redhat.com>
2021-09-03 14:39:19 +0300

    Skipping flaky test id 1530 for informing jobs
    Signed-off-by: orenc1 <ocohen@redhat.com>

--- hack/test-cnv.sh
+++ hack/test-cnv.sh
@@ -64,7 +64,10 @@ skip_tests+=('test_id:4659')
 # Skipping "Delete a VirtualMachineInstance with ACPI and 0 grace period seconds" due to a bug
 skip_tests+=('test_id:1652')
 
-
+if [ "$PRODUCTION_RELEASE" = "true" ]; then
+  # Skipping flaky test for OCP Informing Jobs.
+  skip_tests+=('test_id:1530')
+fi
 skip_regex=$(printf '(%s)|' "${skip_tests[@]}")
 skip_arg=$(printf -- '--ginkgo.skip=%s' "${skip_regex:0:-1}")
 
