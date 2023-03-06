fbb7f770365f78afbcb47423315cb38385092b42
stirabos <stirabos@redhat.com>
2023-03-06 16:56:24 +0100

    Bootstrap 4.14
    - Fix the upgrade logic:
    if not on production catalog source,
    start from the CSV version replaced by
    the head of the brew registry and upgrade
    to the head of the brew registry
    - Fix minor bugs
    - Boostrap 4.14
    - Amend 4.13 conf pointing to the staging version
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- hack/test-cnv.sh
+++ hack/test-cnv.sh
@@ -10,13 +10,8 @@ VIRT_OPERATOR_IMAGE=$(oc get deployment virt-operator -n ${TARGET_NAMESPACE} -o 
 if [ "$PRODUCTION_RELEASE" = "false" ]; then
   # In case of a pre-release build, use the brew registry for the virt-operator image pullspec
   VIRT_OPERATOR_IMAGE=${VIRT_OPERATOR_IMAGE//registry.redhat.io\/container-native-virtualization\//brew.registry.redhat.io\/rh-osbs\/container-native-virtualization-}
-  MACHINETYPE=$(oc get kv kubevirt-kubevirt-hyperconverged -n ${TARGET_NAMESPACE} -o jsonpath='{.spec.configuration.machineType}')
-  if [[ "$MACHINETYPE" == *"rhel9"* ]]
-  then
-    VIRT_OPERATOR_IMAGE=${VIRT_OPERATOR_IMAGE/virt-operator/virt-operator-rhel9}
-  fi
 fi
-KUBEVIRT_TAG=$(oc image info -a /tmp/authfile.new ${VIRT_OPERATOR_IMAGE} -o json | jq '.config.config.Labels["upstream-version"]')
+KUBEVIRT_TAG=$(oc image info -a /tmp/authfile.new ${VIRT_OPERATOR_IMAGE} -o json --filter-by-os=linux/amd64 | jq '.config.config.Labels["upstream-version"]')
 KUBEVIRT_RELEASE=v$(echo ${KUBEVIRT_TAG} | awk -F '-' '{print $1}' | tr -d '"')
 if [[ ${KUBEVIRT_TAG} == *"rc"* ]] || [[ ${KUBEVIRT_TAG} == *"alpha"* ]]; then
   KUBEVIRT_TESTS_URL=https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_RELEASE}/tests.test
@@ -46,7 +41,20 @@ BIN_DIR="$(pwd)/_out" && mkdir -p "${BIN_DIR}"
 export BIN_DIR
 
 TESTS_BINARY="$BIN_DIR/tests.test"
-curl -Lo "$TESTS_BINARY" "https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_RELEASE}/tests.test"
+######
+# hack to run a fixed version of Kubevirt tests
+# please remove this hack once
+# - https://github.com/kubevirt/kubevirt/pull/9683
+# - https://github.com/kubevirt/kubevirt/pull/9684
+# are properly consumed
+#curl -Lo "$TESTS_BINARY" "https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_RELEASE}/tests.test"
+if [[ "$KUBEVIRT_RELEASE" == "v1.0.0-alpha.0" ]]; then
+    echo "Using a fixed version of kubevirt tests"
+    curl -Lo "$TESTS_BINARY" "https://github.com/tiraboschi/kubevirt/releases/download/v1.0.0-alpha.0-fix/tests.test"
+else
+    curl -Lo "$TESTS_BINARY" "https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_RELEASE}/tests.test"
+fi
+######
 chmod +x "$TESTS_BINARY"
 
 echo "create testing infrastructure"
@@ -56,7 +64,7 @@ echo "waiting for testing infrastructure to be ready"
 oc wait deployment cdi-http-import-server -n "${TARGET_NAMESPACE}" --for condition=Available --timeout=10m
 oc wait pods -l "kubevirt.io=disks-images-provider" -n "${TARGET_NAMESPACE}" --for condition=Ready --timeout=20m
 
-skip_tests+=('\[QUARANTINE]')
+skip_tests+=('\[QUARANTINE\]')
 skip_tests+=('Slirp Networking')
 skip_tests+=('with CPU spec')
 skip_tests+=('with TX offload disabled')
@@ -88,6 +96,7 @@ skip_tests+=('test_id:1652')
 
 # Skipping a few unrealiable tests
 skip_tests+=('rfe_id:273')
+skip_tests+=('test_id:1304')
 skip_tests+=('test_id:1615')
 skip_tests+=('test_id:1616')
 skip_tests+=('test_id:1617')
@@ -95,25 +104,48 @@ skip_tests+=('test_id:1618')
 skip_tests+=('test_id:1626')
 skip_tests+=('test_id:1651')
 skip_tests+=('test_id:1657')
+skip_tests+=('test_id:2188')
 skip_tests+=('test_id:2190')
+skip_tests+=('test_id:3007')
 skip_tests+=('test_id:3178')
 skip_tests+=('test_id:3180')
 skip_tests+=('test_id:3182')
 skip_tests+=('test_id:3184')
 skip_tests+=('test_id:3185')
 skip_tests+=('test_id:3199')
+skip_tests+=('test_id:3312')
 skip_tests+=('test_id:4119')
+skip_tests+=('test_id:4136')
 skip_tests+=('test_id:4622')
 skip_tests+=('test_id:6993')
+skip_tests+=('test_id:6311')
+skip_tests+=('test_id:7164')
 skip_tests+=('test_id:7679')
-skip_tests+=('[Serial] Should leave a failed VMI')
+skip_tests+=('\[Serial\] Should leave a failed VMI')
 skip_tests+=('VirtualMachine crash loop backoff should backoff attempting to create a new VMI when')
+skip_tests+=('Using expand command')
+skip_tests+=('Using virtctl interface')
+skip_tests+=('repeately starting vmis')
+skip_tests+=('Prometheus Endpoints')
+
 
 skip_regex=$(printf '(%s)|' "${skip_tests[@]}")
 skip_arg=$(printf -- '--ginkgo.skip=%s' "${skip_regex:0:-1}")
 
 
 mkdir -p "${ARTIFACT_DIR}"
+
+if [[ "$OCP_VERSION" == "4.10" ]];
+then
+  GINKGO_FLAKE="--ginkgo.flakeAttempts=3"
+  GINKGO_NOCOLOR="--ginkgo.noColor"
+  GINKGO_SLOW="--ginkgo.slowSpecThreshold=60"
+else
+  GINKGO_FLAKE="--ginkgo.flake-attempts=3"
+  GINKGO_NOCOLOR="--ginkgo.no-color"
+  GINKGO_SLOW="--ginkgo.slow-spec-threshold=60s"
+fi
+
 
 echo "starting tests"
 ${TESTS_BINARY} \
@@ -123,9 +155,9 @@ ${TESTS_BINARY} \
     -junit-output="${ARTIFACT_DIR}/junit.functest.xml" \
     -kubeconfig="$KUBECONFIG" \
     -ginkgo.focus='(rfe_id:1177)|(rfe_id:273)|(rfe_id:151)' \
-    -ginkgo.noColor \
+    "${GINKGO_NOCOLOR}" \
     -ginkgo.seed=0 \
-    -ginkgo.slowSpecThreshold=60 \
+    "${GINKGO_SLOW}" \
     -ginkgo.v \
     -ginkgo.trace \
     -oc-path="$(which oc)" \
@@ -133,5 +165,7 @@ ${TESTS_BINARY} \
     -utility-container-prefix=quay.io/kubevirt \
     -test.timeout=3h \
     -test.v \
-    -ginkgo.flakeAttempts=3 \
+    -ginkgo.skip="(should find one leading virt-controller and two ready)" \
+    -ginkgo.skip="(stable functionality by repeately starting)" \
+    "${GINKGO_FLAKE}" \
     "${skip_arg}"
