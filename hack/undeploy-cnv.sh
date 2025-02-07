a76899f299a77ef18c955335c803cbcc21f3cb01
Denis Ollier <dollierp@redhat.com>
2025-02-07 12:11:15 +0100

    Support deploying CNV from a custom catalog
    Signed-off-by: Denis Ollier <dollierp@redhat.com>

--- hack/undeploy-cnv.sh
+++ hack/undeploy-cnv.sh
@@ -6,4 +6,4 @@ oc delete HyperConverged -n "${TARGET_NAMESPACE}" kubevirt-hyperconverged
 oc delete OperatorGroup -n "${TARGET_NAMESPACE}" openshift-cnv-group
 oc delete Subscription -n "${TARGET_NAMESPACE}" kubevirt-hyperconverged
 oc delete ns "${TARGET_NAMESPACE}"
-oc delete CatalogSource -n "openshift-marketplace" brew-catalog-source
+oc delete CatalogSource -n "openshift-marketplace" cnv-catalog-source
