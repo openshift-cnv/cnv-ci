c4ed61f0dc850616408f8643c998720a52926deb
Rabin Yasharzadehe <rabin@redhat.com>
2025-02-20 22:10:38 +0200

    Refactor CNV deployment
    Signed-off-by: Rabin Yasharzadehe <rabin@redhat.com>

--- hack/deploy-cnv.sh
+++ hack/deploy-cnv.sh
@@ -13,48 +13,61 @@ function cleanup() {
     exit $rv
 }
 
+function get_cnv_catalog_image() {
+    # Environment variable has higher priority
+    if [ -n "${CNV_CATALOG_IMAGE-}" ]; then
+        return
+    fi
+
+    # Fallback to the image requested in the Prow job spec if any
+    if [ -n "${PROW_JOB_ID-}" ]; then
+        CNV_CATALOG_IMAGE=$(
+          curl -fsSL https://prow.ci.openshift.org/prowjob?prowjob=${PROW_JOB_ID} \
+            | sed -nr '/name: CNV_CATALOG_IMAGE/ { n; s|\s+value: (.*)|\1|p }'
+        )
+    fi
+
+    # Ultimate fallback, get image from mapping file
+    if [ -z "${CNV_CATALOG_IMAGE-}" ]; then
+        eval "$(jq -r '."'"${CNV_VERSION}"'" | to_entries[] | .key+"="+.value' version-mapping.json)"
+
+        # These variables are set when the eval above is executed
+        CNV_CATALOG_IMAGE=$index_image
+
+        # shellcheck disable=SC2154
+        echo "Using bundle_version: $bundle_version"
+
+        STARTING_CSV=${bundle_version%-*}
+        STARTING_CSV=${STARTING_CSV%.rhel*}
+    fi
+}
+
 trap "cleanup" INT TERM EXIT
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "OCP_VERSION: $OCP_VERSION"
 
+CNV_VERSION=${CNV_VERSION:-${OCP_VERSION}}
+CNV_SUBSCRIPTION_CHANNEL=${CNV_SUBSCRIPTION_CHANNEL:-stable}
+
 oc create ns "${TARGET_NAMESPACE}"
 
 if [ "$PRODUCTION_RELEASE" = "true" ]; then
-    echo "creating subscription"
-    oc create -f - <<EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: kubevirt-hyperconverged
-  namespace: "${TARGET_NAMESPACE}"
-  labels:
-    operators.coreos.com/kubevirt-hyperconverged.openshift-cnv: ''
-spec:
-  channel: stable
-  installPlanApproval: Automatic
-  name: kubevirt-hyperconverged
-  source: redhat-operators
-  sourceNamespace: openshift-marketplace
-EOF
-
+    CNV_CATALOG_SOURCE='redhat-operators'
 else
-    eval "$(jq -r '."'"$OCP_VERSION"'" | to_entries[] | .key+"="+.value' version-mapping.json)"
-    
-    # These variables are set when the eval above is executed
-    # shellcheck disable=SC2154
-    echo "Using index_image: $index_image"
-    # shellcheck disable=SC2154
-    echo "Using bundle_version: $bundle_version"
+    CNV_CATALOG_SOURCE='cnv-catalog-source'
+    get_cnv_catalog_image
 
-    echo "setting up brew catalog source"
-    "$SCRIPT_DIR"/create-brew-catalogsource.sh
+    # shellcheck disable=SC2154
+    echo "Using index_image: ${CNV_CATALOG_IMAGE}"
 
-    STARTING_CSV=${bundle_version%-*}
-    STARTING_CSV=${STARTING_CSV/.rhel9/}
-    echo "creating subscription"
-    oc create -f - <<EOF
+    echo "setting up CNV catalog source"
+    "$SCRIPT_DIR"/create-cnv-catalogsource.sh "${CNV_CATALOG_IMAGE}"
+fi
+
+echo "creating subscription"
+oc create -f - <<EOF
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
@@ -63,14 +76,13 @@ metadata:
   labels:
     operators.coreos.com/kubevirt-hyperconverged.openshift-cnv: ''
 spec:
-  channel: stable
+  channel: ${CNV_SUBSCRIPTION_CHANNEL}
   installPlanApproval: Automatic
   name: kubevirt-hyperconverged
-  source: brew-catalog-source
+  source: ${CNV_CATALOG_SOURCE}
   sourceNamespace: openshift-marketplace
-  startingCSV: kubevirt-hyperconverged-operator.${STARTING_CSV}
+  ${STARTING_CSV:+startingCSV: kubevirt-hyperconverged-operator.${STARTING_CSV}}
 EOF
-fi
 
 echo "creating operator group"
 oc create -f - <<EOF
