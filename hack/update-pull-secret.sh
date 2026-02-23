679c02df1dcf2658cd877068d866a78a7ee5a628
Oren Cohen <ocohen@redhat.com>
2026-02-23 13:16:49 +0200

    [release-4.21] bring back brew registry pull-secret
    this is needed in order to use the iib index image (e.g. brew.registry.redhat.io/rh-osbs/iib:1107792)
    
    Signed-off-by: Oren Cohen <ocohen@redhat.com>

--- hack/update-pull-secret.sh
+++ hack/update-pull-secret.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+function get_brew_auth() {
+    echo -n '{ "brew.registry.redhat.io": { "auth": "'"$(echo "$BREW_IMAGE_REGISTRY_USERNAME:$(<$BREW_IMAGE_REGISTRY_TOKEN_PATH)" | tr -d '\n' | base64 -i -w 0)"'" } }'
+}
+
 function get_konflux_auth() {
     echo -n '{ "quay.io/openshift-virtualization": { "auth": "'"$(echo "$KONFLUX_REGISTRY_USERNAME:$(<$KONFLUX_REGISTRY_TOKEN_PATH)" | tr -d '\n' | base64 -i -w 0)"'" } }'
 }
@@ -13,7 +17,7 @@ echo "getting authfile from cluster"
 oc get secret/pull-secret -n openshift-config -o json | jq -r '.data.".dockerconfigjson"' | base64 -d >"$authfile"
 
 echo "injecting credentials into authfile"
-jq -c '.auths + '"$(get_konflux_auth)"' | {"auths": .}' "$authfile" >"${authfile}.new"
+jq -c '.auths + '"$(get_brew_auth)"' + '"$(get_konflux_auth)"' | {"auths": .}' "$authfile" >"${authfile}.new"
 
 echo "updating cluster pull secret from authfile"
 oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson="${authfile}.new"
