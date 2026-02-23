8340d067f04662de89c3db75744aec8cd2dd3030
Oren Cohen <ocohen@redhat.com>
2026-02-23 16:44:28 +0200

    [4.21] Fix IDMS for HCO bundle image
    stage registry (konflux) has the -rhel9 suffix for this image, while the production registry doesn't.
    updating the IDMS for this exception.
    
    Signed-off-by: Oren Cohen <ocohen@redhat.com>

--- hack/set-imagedigestmirrorset.sh
+++ hack/set-imagedigestmirrorset.sh
@@ -14,6 +14,10 @@ spec:
     mirrors:
       - brew.registry.redhat.io/container-native-virtualization
       - quay.io/openshift-virtualization/konflux-builds/v4-21
+  - source: registry.redhat.io/container-native-virtualization/hco-bundle-registry
+    mirrors:
+      - brew.registry.redhat.io/rh-osbs/container-native-virtualization-hco-bundle-registry
+      - quay.io/openshift-virtualization/konflux-builds/v4-21/hco-bundle-registry-rhel9  
 EOF
 
 echo "waiting for update to start"
