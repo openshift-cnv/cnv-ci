051ddc718f060e41c087a30888f7c7fa48a8b7eb
Rabin Yasharzadehe <rabin@redhat.com>
2026-04-23 14:28:22 +0300

    Backport !154 - Enable featureGates RebootPolicy
    Signed-off-by: Rabin Yasharzadehe <rabin@redhat.com>

--- hack/wait-for-hco.sh
+++ hack/wait-for-hco.sh
@@ -4,14 +4,17 @@ set -euxo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+HCO_KIND="HyperConverged"
+HCO_CR="kubevirt-hyperconverged"
+
 echo "waiting for hco-operator deployment to be created"
-$SCRIPT_DIR/retry.sh 30 10 "oc get deployment hco-operator -n $TARGET_NAMESPACE"
+$SCRIPT_DIR/retry.sh 30 10 "oc get deployment hco-operator -n ${TARGET_NAMESPACE}"
 
 echo "waiting for hco-webhook deployment to be created"
-$SCRIPT_DIR/retry.sh 30 10 "oc get deployment hco-webhook -n $TARGET_NAMESPACE"
+$SCRIPT_DIR/retry.sh 30 10 "oc get deployment hco-webhook -n ${TARGET_NAMESPACE}"
 
 echo "waiting for hco-operator and hco-webhook to be ready"
-oc wait deployment hco-operator hco-webhook -n $TARGET_NAMESPACE --for condition=Available --timeout=30m
+oc wait deployment hco-operator hco-webhook -n ${TARGET_NAMESPACE} --for condition=Available --timeout=30m
 
 # Wait a few more seconds for the hco-webhook Service and Endpoints objects
 # to be updated and propagated through the system, to prevent connection errors to the webhook.
@@ -21,16 +24,16 @@ echo "waiting for HyperConverged operator CRD to be created"
 $SCRIPT_DIR/retry.sh 30 10 "oc get crd hyperconvergeds.hco.kubevirt.io"
 
 echo "checking if HyperConverged operator CR already exists"
-if [ "$(oc get HyperConverged kubevirt-hyperconverged -n $TARGET_NAMESPACE --no-headers | wc -l)" -eq 0 ]; then
+if [ "$(oc get "${HCO_KIND}" "${HCO_CR}" -n ${TARGET_NAMESPACE} --no-headers | wc -l)" -eq 0 ]; then
 
 	echo "creating HyperConverged operator CR"
-	
+
 	oc create -f - <<EOF
 apiVersion: hco.kubevirt.io/v1beta1
 kind: HyperConverged
 metadata:
-  name: kubevirt-hyperconverged
-  namespace: $TARGET_NAMESPACE
+  name: ${HCO_CR}
+  namespace: ${TARGET_NAMESPACE}
 spec:
   BareMetalPlatform: true
 EOF
@@ -38,4 +41,15 @@ EOF
 fi
 
 echo "waiting for HyperConverged operator to be available"
-oc wait HyperConverged kubevirt-hyperconverged -n $TARGET_NAMESPACE --for condition=Available --timeout=30m
+oc wait "${HCO_KIND}" "${HCO_CR}" -n ${TARGET_NAMESPACE} --for condition=Available --timeout=30m
+
+echo "Enable KubeVirt RebootPolicy feature gate (interop e2e exercises reboot policy specs)."
+oc annotate "${HCO_KIND}" "${HCO_CR}" \
+  --namespace="${TARGET_NAMESPACE}" \
+  --overwrite \
+  kubevirt.kubevirt.io/jsonpatch='[
+    {"op": "add", "path": "/spec/configuration/developerConfiguration/featureGates/-", "value": "RebootPolicy"}
+  ]'
+
+echo "waiting for HyperConverged operator to be available (again)"
+oc wait "${HCO_KIND}" "${HCO_CR}" -n "${TARGET_NAMESPACE}" --for=condition=Available --timeout=15m
