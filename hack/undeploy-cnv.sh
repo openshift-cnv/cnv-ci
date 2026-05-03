1d33063f2e5acecb1ce6572d4787b085f018c41e
Rabin Yasharzadehe <rabin@redhat.com>
2026-05-03 15:29:04 +0300

    Use explicit versioned kind for HyperConverged
    Pin HyperConverged kind to v1beta1 (hyperconvergeds.v1beta1.hco.kubevirt.io)
    to prevent breakage when the v1 API is introduced.
    
    Mirrors contra/cnv-qe-automation MR !23077.
    
    Signed-off-by: Rabin Yasharzadehe <rabin@redhat.com>

--- hack/undeploy-cnv.sh
+++ hack/undeploy-cnv.sh
@@ -2,7 +2,9 @@
 
 set -euxo pipefail
 
-oc delete HyperConverged -n "${TARGET_NAMESPACE}" kubevirt-hyperconverged
+HCO_KIND="hyperconvergeds.v1beta1.hco.kubevirt.io"
+
+oc delete "${HCO_KIND}" -n "${TARGET_NAMESPACE}" kubevirt-hyperconverged
 oc delete OperatorGroup -n "${TARGET_NAMESPACE}" openshift-cnv-group
 oc delete Subscription -n "${TARGET_NAMESPACE}" kubevirt-hyperconverged
 oc delete ns "${TARGET_NAMESPACE}"
